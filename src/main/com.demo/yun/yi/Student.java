3eebee0281600685d5142bfb6dc736af6e4f1957
leidinghappy <leidinghappy@163.com>
2017-01-15 16:53:34 +0800

    测试

--- src/main/com.demo/yun/yi/Student.java
+++ src/main/com.demo/yun/yi/Student.java
@@ -6,7 +6,7 @@ package yun.yi;
 public class Student {
 
     private String id;
-
+    private String dingLei;
     private String name;
     public Student(){}
     public Student (String id,String name){
